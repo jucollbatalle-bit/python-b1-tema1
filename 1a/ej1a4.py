d1af93fe55e73c4fff4dc99df81bd53a680571d1
jucollbatalle-bit <jucollbatalle@uoc.edu>
2025-12-12 21:35:20 +0100

    Update ej1a4

--- 1a/ej1a4.py
+++ 1a/ej1a4.py
@@ -34,9 +34,11 @@ Exemple:
 '''
 
 def count_vowels(text_chain:str):
-    # Write here your code
-    pass
+    vowels = "aeiouAEIOU"
+    count = 0
+    for char in text_chain:
+        if char in vowels:
+            count += 1
+    return count
 
-# Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script 
-# Si vols provar el teu codi, descomenta les línies següents i executa l'script
-# print(count_vowels("Hello world, this is an example."))
+print(count_vowels("Hello world, this is an example."))
