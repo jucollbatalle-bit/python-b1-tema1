bbc07ee9fb0b9af9263c62662d972e0ddb665f96
jucollbatalle-bit <jucollbatalle@uoc.edu>
2025-12-13 19:40:34 +0100

    update ej1c2

--- 1c/ej1c2.py
+++ 1c/ej1c2.py
@@ -36,11 +36,8 @@ Exemple
 
 
 def invert_list(lst):
-    # Write here your code
-    pass
-
-
-# Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
-# Si vols provar el teu codi, descomenta les línies següents i executa l'script
-# lst = [1, 20, 3, 40, 5]
-#print(invert_list(lst))
+    if len(lst) <= 1:
+        return lst
+    else:
+        return invert_list(lst[1:]) + [lst[0]]
+print(f"Resultado final: {invert_list([1, 2, 3, 4, 5])}")
