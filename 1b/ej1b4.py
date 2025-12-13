ed7b17c3b44dc320efe3e259b82e7d87dae08660
jucollbatalle-bit <jucollbatalle@uoc.edu>
2025-12-13 18:14:34 +0100

    Update ej1b4.py

--- 1b/ej1b4.py
+++ 1b/ej1b4.py
@@ -65,9 +65,9 @@ Exemple:
 import numpy as np
 
 def results(list_numbers):
-    # Write here your code
-    pass
-
-# Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script 
-# Si vols provar el teu codi, descomenta les línies següents i executa l'script
-# results([1, 2, 10, -5, 0, 9.55, 74.825, 55, 8, 42])
+    desviacion_estandar = np.std(list_numbers)
+    result_1 = round(desviacion_estandar, 2)
+    promedio = np.mean(list_numbers)
+    result_2 = round(promedio, 2)
+    return result_1, result_2
+results([1, 2, 10, -5, 0, 9.55, 74.825, 55, 8, 42])
